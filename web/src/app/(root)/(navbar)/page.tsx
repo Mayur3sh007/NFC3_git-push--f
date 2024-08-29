d7923eff5d94f83fe79557286623f1848c2bb9b8
Anish Awasthi <anishawasthi16@gmail.com>
2024-08-30 03:04:13 +0530

    hero page

--- web/src/app/(root)/(navbar)/page.tsx
+++ web/src/app/(root)/(navbar)/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Auth from "@/components/auth/Auth";
-import Hero from "@/components/others/Hero";
 import LatestNews from "@/components/others/LatestNews";
+import Hero from "@/components/others/Hero";
 
 const page = () => {
   return (
